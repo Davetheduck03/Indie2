ea90b6e81f9392640e61a0e16b796ac05fa1aba0
Davetheduck03 <156407317+Davetheduck03@users.noreply.github.com>
2024-11-25 20:16:47 +0700

    typeshit

--- Assets/Scripts/Enemy/NormalEnemy.cs
+++ Assets/Scripts/Enemy/NormalEnemy.cs
@@ -5,12 +5,8 @@ using UnityEngine;
 
 public class NormalEnemy : BaseEnemy
 {
-
     public override void Initialize(float health, float speed, float damage, float coin)
     {
         base.Initialize(100, 2, 1, 10);
     }
-
-
-
 }
