bc1ffe91e2be60de511aaa275e522b7e8e38ac2d
Davetheduck03 <156407317+Davetheduck03@users.noreply.github.com>
2024-11-26 14:38:42 +0700

    Door completed

--- Assets/Scripts/Door/Door.cs
+++ Assets/Scripts/Door/Door.cs
@@ -3,20 +3,44 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Door : MonoBehaviour
+public class Door : MonoBehaviour, IInteractable
 {
     [SerializeField] Collider2D doorCollider;
     [SerializeField] Animator uDoorAnimator;
     [SerializeField] Animator lDoorAnimator;
+    [SerializeField] GameObject door;
+    [SerializeField] bool playerInside = false;
+    [SerializeField] LayerMask layerPlayer;
+
+
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        PlayerCheck();
+        if (playerInside == true && Input.GetKeyDown(KeyCode.E))
         {
-            Destroy(doorCollider);
-
-            uDoorAnimator.SetTrigger("Open");
-            lDoorAnimator.SetTrigger("Open");
+            Interact();
         }
+    }
+    public void Interact()
+    {
+        Destroy(doorCollider);
+
+        uDoorAnimator.SetTrigger("Open");
+        lDoorAnimator.SetTrigger("Open");
+    }
+
+    private void PlayerCheck()
+    {
+         Vector2 doorPos = door.transform.position;
+         if(Physics2D.OverlapCircle(doorPos, 2f, layerPlayer))
+         {
+                playerInside = true;
+         }
+        else
+        {
+            playerInside = false;
+        }
+
     }
 
 }
