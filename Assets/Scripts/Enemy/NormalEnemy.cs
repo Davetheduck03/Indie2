71cf99bd49bdc438ab97e2bdf979fb16910a0592
Davetheduck03 <khanh.tq@st.buv.edu.vn>
2025-03-04 12:53:19 +0700

    osIEDHngpoiASDFNgoinasdfgoinsdzfokign

--- Assets/Scripts/Enemy/NormalEnemy.cs
+++ Assets/Scripts/Enemy/NormalEnemy.cs
@@ -1,17 +1,11 @@
 using System.Collections;
-using UnityEngine;
+
 
 public class NormalEnemy : BaseEnemy
 {
+
     public override void Initialize(float health, float speed, float damage)
     {
         base.Initialize(100, 1f, 1);
     }
-
-    private void FixedUpdate()
-    {
-       
-    }
-
-    
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -21,7 +21,6 @@ public class Player : MonoBehaviour
 
     public GameObject bulletPrefab;
 
-
     Animator anim;
     private Vector2 lastMoveDirection;
 
