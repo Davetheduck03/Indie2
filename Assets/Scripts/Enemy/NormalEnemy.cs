b1dacb70369df043a896ceb493edf6c77e43bda9
Davetheduck03 <khanh.tq@st.buv.edu.vn>
2025-03-05 17:00:32 +0700

    a

--- Assets/Scripts/Enemy/NormalEnemy.cs
+++ Assets/Scripts/Enemy/NormalEnemy.cs
@@ -4,16 +4,8 @@ using UnityEngine.AI;
 
 public class NormalEnemy : BaseEnemy
 {
-    public Player player;
-    NavMeshAgent agent;
     public override void Initialize(float health, float speed, float damage)
     {
         base.Initialize(100, 1f, 1);
-        agent = GetComponent<NavMeshAgent>();
-    }
-
-    private void Update()
-    {
-        agent.SetDestination(player.transform.localPosition);
     }
 }
